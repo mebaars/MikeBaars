a8372cdce7e787eb78fedd940592db73e27ac073
mebaars <m.e.baars@gmail.com>
2024-05-28 23:45:48 -0600

    update from local

--- assets/js/iframe_script.js
+++ assets/js/iframe_script.js
@@ -1,9 +1,11 @@
+// script.js
 document.addEventListener('DOMContentLoaded', function() {
     var modal = document.getElementById("iframeModal");
     var btn = document.getElementById("openModalButton");
-    
+    var span = document.getElementsByClassName("close")[0];
+
     if (!modal) {
-        console.error('Element with id "myModal" not found.');
+        console.error('Element with id "iframeModal" not found.');
     }
 
     if (!btn) {
@@ -13,6 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if (modal && btn) {
         btn.onclick = function() {
             modal.style.display = "block";
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
         }
 
         window.onclick = function(event) {
